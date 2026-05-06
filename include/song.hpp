98526c00e34a13290c35b07158a9628d318d087e
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-05-06 22:35:04 +0800

    feat(songs): auto incrementing ID for each new song

--- include/song.hpp
+++ include/song.hpp
@@ -7,13 +7,12 @@ class Song {
    private:
     std::string name;
     std::string file_loc;
-    int length_s;
-    int ID;
+    int length_s;        // song length (in seconds)
+    static int counter;  // static auto incrementation helper
+    int ID;              // auto incrementation value + 1 on creation
 
    public:
     Song(const std::string& name, const std::string& location, const int& length_seconds);
-    std::string render(void) const;
-    const int get_ID(void);
+    std::string to_string(void) const;
+    int get_ID(void) const;
 };
-
-// auto increment song ID when a new one is made
--- src/song.cpp
+++ src/song.cpp
@@ -1,11 +1,13 @@
 #include "song.hpp"
 
-Song::Song(const std::string& song_name, const std::string& location, const int& length_seconds) : name(song_name), file_loc(location), length_s(length_seconds) {}
+Song::Song(const std::string& song_name, const std::string& location, const int& length_seconds) : name(song_name), file_loc(location), length_s(length_seconds) {
+    ID = ++counter;
+}
 
-std::string Song::render() const {
+std::string Song::to_string() const {
     return name;
 }
 
-const int Song::get_ID() {
+int Song::get_ID() const {
     return ID;
 }
