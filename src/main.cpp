9991c179726c7313e45d38ccf18b3afe96f585d4
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-06-03 23:39:47 +0800

    wip(UI): intergrating new database into display

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
 #include "song.hpp"
 #include "storage.hpp"
 
+#define playlists_window_width 30
+#define playlists_starting_cols_across 3
+#define songs_window_width 50
+#define songs_starting_cols_across 33
+
 enum Mode {
     PLAYLIST,
     SONG,
@@ -19,8 +24,6 @@ void render_window(
     WINDOW* window,
     const std::vector<T>& list,
     int selected_item) {
-    std::string name;
-
     werase(window);  // clear playlist window
 
     wattron(window, COLOR_PAIR(1));
@@ -28,19 +31,24 @@ void render_window(
     wattroff(window, COLOR_PAIR(1));
     mvwprintw(window, 0, 2, "%s", title);  // playlist word still white
 
+    std::string name;
     for (int i = 0; i < (int)list.size(); i++) {
         if (i == selected_item) {
             wattron(window, COLOR_PAIR(2));
         }
 
-        name = list.at(i).to_string();
+        if (title == "Playlists") {
+            name = list.at(i).to_string();
+        } else if (title == "Songs") {
+            name = list.at(i).to_string();
+        }
+
         mvwprintw(window, (i + 1), 3, "%s", name.c_str());
 
         if (i == selected_item) {
             wattroff(window, COLOR_PAIR(2));
         }
     }
-
     // refresh once per frame
     wrefresh(window);
 }
@@ -56,50 +64,22 @@ int main() {
     }
     config_t config = *opt_config;
 
-    std::string music_dir = std::get<std::string>(config.at("music_dir"));
-
-    // std::cout << "files main can see" << std::endl;
-    // for (const auto& entry : fs::directory_iterator(".")) {
-    //     std::cout << entry.path() << std::endl;
-    // }
-
-    // std::vector<Playlist> playlists;
-
     // setup
-    // initscr();      // Start ncurses mode
-    // start_color();  // turns on colour
-    // cbreak();
-    // noecho();
+    initscr();      // Start ncurses mode
+    start_color();  // turns on colour
+    cbreak();
+    noecho();
 
-    // // colour pairs
-    // init_pair(1, COLOR_BLUE, COLOR_BLACK);
-    // init_pair(2, COLOR_BLACK, COLOR_WHITE);
+    // colour pairs
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
 
-    // int screen_y, screen_x;
-    // getmaxyx(stdscr, screen_y, screen_x);
-
-    ////////////////////////////////////
-    // Prep default data
-    // Song song1("this_place.mp3", 654);
-    // Song song2("nowhere.mp3", 324);
-    // Song song3("everywhere.mp3", 68);
-
-    // Playlist pl("Playlist:1");
-    // pl.add(song1);
-    // pl.add(song2);
-    // database.playlists.push_back(pl);
-
-    // Playlist pl2("Playlist:2");
-    // pl2.add(song2);
-    // pl2.add(song3);
-    // database.playlists.push_back(pl2);
+    int screen_y, screen_x;
+    getmaxyx(stdscr, screen_y, screen_x);
 
     Storage storage{};  // () here causes an error c++ thinks it is a function
-    // storage.add(pl2);
-    // storage.add(song1);
-    // storage.add(song2);
-    /////////////////////////////////////
 
+    std::string music_dir = std::get<std::string>(config.at("music_dir"));
     std::optional<Database> odb = storage.load(music_dir);
     if (!odb.has_value()) {
         std::cerr << "Failed to load database\n";
@@ -109,73 +89,81 @@ int main() {
 
     // main playlist section
     // newwin(lines down, cols across, starting lines down, starting cols across)
-    // WINDOW* playlist_window = newwin((screen_y - 3), 30, 1, 3);  // Playlists
-    // keypad(playlist_window, TRUE);                               // enable keyboard on playlist window
+    WINDOW* playlist_window = newwin(
+        (screen_y - 3),
+        playlists_window_width,
+        1,
+        playlists_starting_cols_across);  // Playlists
+    keypad(playlist_window, TRUE);        // enable keyboard on playlist window
 
-    // WINDOW* song_window = newwin((screen_y - 3), 30, 1, 33);  // songs window
-    // keypad(song_window, TRUE);
+    WINDOW* song_window = newwin(
+        (screen_y - 3),
+        songs_window_width,
+        1,
+        songs_starting_cols_across);  // songs window
+    keypad(song_window, TRUE);
 
-    // refresh();  // create playlists & songs window on screen
+    refresh();  // create playlists & songs window on screen
 
-    // int selected_playlist = 0;
-    // int selected_song = 0;
-    // int input;
-    // Mode current_mode = PLAYLIST;
+    int selected_playlist = 0;
+    int selected_song = 0;
+    int input;
+    Mode current_mode = PLAYLIST;
 
-    // // initial render
-    // render_window("Playlists", playlist_window, playlists, selected_playlist);
-    // render_window("Songs", song_window, (playlists.at(selected_playlist).get_songs()), selected_song);
+    // initial render
+    render_window("Playlists", playlist_window, db.playlists, selected_playlist);
+    render_window("Songs", song_window, (db.playlists.at(selected_playlist).get_songs()), selected_song);
 
-    // // main loop
-    // while ((input = wgetch(
-    //             (current_mode == PLAYLIST)
-    //                 ? playlist_window
-    //                 : song_window)) != 'q') {  // get key press
-    //     switch (input) {
-    //         case KEY_UP:
-    //             if (current_mode == PLAYLIST) {
-    //                 if (selected_playlist > 0) {
-    //                     selected_playlist--;
-    //                 }
+    // main loop
+    while ((input = wgetch(
+                (current_mode == PLAYLIST)
+                    ? playlist_window
+                    : song_window)) != 'q') {  // get key press
+        switch (input) {
+            case KEY_UP:
+                if (current_mode == PLAYLIST) {
+                    if (selected_playlist > 0) {
+                        selected_playlist--;
+                    }
 
-    //             } else {
-    //                 if (selected_song > 0) {
-    //                     selected_song--;
-    //                 }
-    //             }
-    //             break;
+                } else {
+                    if (selected_song > 0) {
+                        selected_song--;
+                    }
+                }
+                break;
 
-    //         case KEY_DOWN:
-    //             if (current_mode == PLAYLIST) {
-    //                 if (selected_playlist < (int)playlists.size() - 1) {
-    //                     selected_playlist++;
-    //                 }
+            case KEY_DOWN:
+                if (current_mode == PLAYLIST) {
+                    if (selected_playlist < (int)db.playlists.size() - 1) {
+                        selected_playlist++;
+                    }
 
-    //             } else {
-    //                 if (selected_song < (int)playlists.at(selected_playlist).get_songs().size() - 1) {
-    //                     selected_song++;
-    //                 }
-    //             }
-    //             break;
+                } else {
+                    if (selected_song < (int)db.playlists.at(selected_playlist).get_songs().size() - 1) {
+                        selected_song++;
+                    }
+                }
+                break;
 
-    //         case 10:  // KEY_ENTER
-    //             // switch window to song navigation
-    //             if (current_mode == PLAYLIST) {
-    //                 current_mode = SONG;
-    //             } else {
-    //                 current_mode = PLAYLIST;
-    //                 selected_song = 0;
-    //             }
-    //             break;
+            case 10:  // KEY_ENTER
+                // switch window to song navigation
+                if (current_mode == PLAYLIST) {
+                    current_mode = SONG;
+                } else {
+                    current_mode = PLAYLIST;
+                    selected_song = 0;
+                }
+                break;
 
-    //         default:
-    //             break;
-    //     }
+            default:
+                break;
+        }
 
-    //     render_window("Playlists", playlist_window, playlists, selected_playlist);
-    //     render_window("Songs", song_window, (playlists.at(selected_playlist).get_songs()), selected_song);
-    // }
+        render_window("Playlists", playlist_window, db.playlists, selected_playlist);
+        render_window("Songs", song_window, (db.playlists.at(selected_playlist).get_songs()), selected_song);
+    }
 
-    // endwin();  // End ncurses mode
+    endwin();  // End ncurses mode
     return 0;
 };
